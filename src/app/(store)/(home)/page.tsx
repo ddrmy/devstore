f9e4a6c32314d2e4bff6afe9074178ba275080ed
ddrmy <gabriel_rti@hotmail.com>
2026-07-13 13:00:19 -0300

    add: fetch products on the home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,67 +1,78 @@
+import { api } from "@/app/data/api"
+import { Product } from "@/app/data/types/product"
+import { Currency } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
 
+
+async function getFeaturedProducts() : Promise<Product[]>{
+
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products
+}
+
+
 export default async function Home() {
+  const [highlightedProduct, ...otherProdycts] = await getFeaturedProducts()
+
+
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={`/product/${highlightedProduct.slug}`}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-ai-side.png"
-          alt="Moletom Ai Side"
+          src={highlightedProduct.image}
+          alt={highlightedProduct.title}
           width={920}
           height={920}
           quality={100}
         />
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 bg-black/60 p-1 border-zinc-500 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="text-sm truncate">{highlightedProduct.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 129,00
+            {highlightedProduct.price.toLocaleString('pt-BR', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
+
+          {otherProdycts.map(product => {
+            return(
+              <Link
+              key={(product.id)}
+        href={`/prodyct/${product.slug}`}
         className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-ia-p-devs.png"
-          alt="Moletom IA Para Devs"
+          src={product.image}
+          alt={product.title}
           width={920}
           height={920}
           quality={100}
         />
         <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 bg-black/60 p-1 border-zinc-500 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
+          <span className="text-sm truncate">{product.title}</span>
           <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 129,00
+            {product.price.toLocaleString('pt-br', {
+              style: 'currency',
+              currency: 'BRL'
+            })}
           </span>
         </div>
       </Link>
-      <Link
-        href="/"
-        className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-      >
-        <Image
-          className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-java.png"
-          alt="Moletom Java"
-          width={920}
-          height={920}
-          quality={100}
-        />
-        <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 bg-black/60 p-1 border-zinc-500 pl-5">
-          <span className="text-sm truncate">Moletom AI Side</span>
-          <span className="flex h-full items-center justify-center rounded-full bg-violet-500 px-4 font-semibold">
-            R$ 129,00
-          </span>
-        </div>
-      </Link>
+            )
+          })}
+      
     </div>
   )
 }
--- src/app/api/products/featured/route.ts
+++ src/app/api/products/featured/route.ts
@@ -4,4 +4,4 @@ export async function GET() {
   const featuredProducts = data.products.filter((product) => product.featured)
 
   return Response.json(featuredProducts)
-}
+} 
